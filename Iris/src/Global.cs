c4599a7fa1afcf59b099d806a818f0942fe565c4
Luca <luke.berger02@gmail.com>
2022-02-25 00:59:45 +0100

    Projektdateien hinzufügen.

---
+++ Iris/src/Global.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Iris
+{
+    static internal class Global
+    {
+        #region Properties and Variables
+        #region Colors
+        public static SolidColorBrush? MaterialDesignDarkBackground { get; private set; }
+        public static SolidColorBrush? MaterialDesignDarkForeground { get; private set; }
+        public static SolidColorBrush? MaterialDesignDarkSeparatorBackground { get; private set; }
+        #endregion
+        #endregion
+
+        #region Constructors
+        static Global()
+        {
+            LoadMaterialDesignColors();
+        }
+        #endregion
+
+        #region Methods
+        #region Private
+        static private void LoadMaterialDesignColors()
+        {
+            MaterialDesignDarkBackground = Application.Current.FindResource("MaterialDesignDarkBackground") as SolidColorBrush;
+            MaterialDesignDarkForeground = Application.Current.FindResource("MaterialDesignDarkForeground") as SolidColorBrush;
+            MaterialDesignDarkSeparatorBackground = Application.Current.FindResource("MaterialDesignDarkSeparatorBackground") as SolidColorBrush;
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ Iris/src/Windows/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System.Windows;
+
+namespace Iris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Properties and Variables
+        private MenuTab SelectedMenuTab 
+        {
+            get => selectedMenuTab;
+            set
+            {
+                if (!selectedMenuTab.Equals(value))
+                {
+                    //Deselect the old active menu tab
+                    switch (selectedMenuTab)
+                    {
+                        case MenuTab.Startpage:
+                            {
+                                StartpageMenuButton.Background = Global.MaterialDesignDarkBackground;
+                                break;
+                            }
+                        case MenuTab.Borrowings:
+                            {
+                                BorrowingsMenuButton.Background = Global.MaterialDesignDarkBackground;
+                                break;
+                            }
+                        case MenuTab.Devices:
+                            {
+                                DevicesMenuButton.Background = Global.MaterialDesignDarkBackground;
+                                break;
+                            }
+                    }
+
+                    //Set the new active menu tab & change content frame
+                    switch (value)
+                    {
+                        case MenuTab.Startpage:
+                            {
+                                StartpageMenuButton.Background = Global.MaterialDesignDarkSeparatorBackground;
+
+                                //TODO: Change COntent frame
+
+                                break;
+                            }
+                        case MenuTab.Borrowings:
+                            {
+                                BorrowingsMenuButton.Background = Global.MaterialDesignDarkSeparatorBackground;
+
+                                //TODO: Change COntent frame
+
+                                break;
+                            }
+                        case MenuTab.Devices:
+                            {
+                                DevicesMenuButton.Background = Global.MaterialDesignDarkSeparatorBackground;
+
+                                //TODO: Change COntent frame
+
+                                break;
+                            }
+                    }
+                }
+
+                selectedMenuTab = value;
+            }
+        }
+        private MenuTab selectedMenuTab = MenuTab.Startpage;
+
+        private enum MenuTab
+        {
+            Startpage,
+            Borrowings,
+            Devices
+        }
+        #endregion
+
+        #region Constructors
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        #region MenuButtons
+        private void StartpageMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMenuTab = MenuTab.Startpage;
+        }
+
+        private void BorrowingsMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMenuTab = MenuTab.Borrowings;
+        }
+
+        private void DevicesMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMenuTab = MenuTab.Devices;
+        }
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
