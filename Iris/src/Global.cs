495d5065eec61f68e6b977a18fba2b6f7f4666c3
Luca <luke.berger02@gmail.com>
2022-02-25 21:54:06 +0100

    Added CurrentUser Logic.

--- Iris/src/Global.cs
+++ Iris/src/Global.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
 
 namespace Iris
 {
@@ -16,12 +14,18 @@ namespace Iris
         public static SolidColorBrush? MaterialDesignDarkForeground { get; private set; }
         public static SolidColorBrush? MaterialDesignDarkSeparatorBackground { get; private set; }
         #endregion
+
+        /// <summary>
+        /// The curred logged in user. (Windows profile)
+        /// </summary>
+        public static string CurrentUser { get; private set; }
         #endregion
 
         #region Constructors
         static Global()
         {
             LoadMaterialDesignColors();
+            CurrentUser = GetUsersFullName();
         }
         #endregion
 
@@ -32,6 +36,44 @@ namespace Iris
             MaterialDesignDarkBackground = Application.Current.FindResource("MaterialDesignDarkBackground") as SolidColorBrush;
             MaterialDesignDarkForeground = Application.Current.FindResource("MaterialDesignDarkForeground") as SolidColorBrush;
             MaterialDesignDarkSeparatorBackground = Application.Current.FindResource("MaterialDesignDarkSeparatorBackground") as SolidColorBrush;
+        }
+
+        /// <summary>
+        /// Get the current users fullname.
+        /// In case of an error, the windows username will be returned.
+        /// </summary>
+        static private string GetUsersFullName()
+        {
+            try
+            {
+                using PrincipalContext? context = new(ContextType.Domain, GetCurrentDomain());
+                using PrincipalSearcher? searcher = new(new UserPrincipal(context));
+                UserPrincipal userPrin = new(context)
+                {
+                    Enabled = true,
+                    SamAccountName = GetWindowsUserName()
+                };
+                searcher.QueryFilter = userPrin;
+
+                return searcher.FindOne().ToString();
+            }
+            catch
+            {
+                return GetWindowsUserName();
+            }
+        }
+
+        static private string GetWindowsUserName()
+        {
+            return Environment.UserName;
+        }
+
+        /// <summary>
+        /// Get the current domain as string.
+        /// </summary>
+        static private string GetCurrentDomain()
+        {
+            return Domain.GetCurrentDomain().ToString();
         }
         #endregion
         #endregion
