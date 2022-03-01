51aaef5c70fd6ea04403f4a2047dd17077ac6af1
Luca <luke.berger02@gmail.com>
2022-03-01 14:23:42 +0100

    Added EditBorrowingsWindow.

---
+++ Iris/src/Windows/EditBorrowingWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Iris.src.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für EditBorrowingWindow.xaml
+    /// </summary>
+    public partial class EditBorrowingWindow : Window
+    {
+        #region Properties and Variables
+
+        #endregion
+
+        #region Constructors
+        public EditBorrowingWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        #region ApplyButton
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region BorrowTakeButton
+        private void BorrowTakeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region FromToDatePicker
+        private void FromToDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        #endregion
+        #endregion
+
+    }
+}
