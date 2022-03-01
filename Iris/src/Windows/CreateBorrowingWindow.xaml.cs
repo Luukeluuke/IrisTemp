0671e8264f9893b6f69e0a27e9dcd798693178a2
Luca <luke.berger02@gmail.com>
2022-03-01 13:52:22 +0100

    Added CreateBorrowingWindow.

---
+++ Iris/src/Windows/CreateBorrowingWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Iris.src.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für CreateBorrowingWindow.xaml
+    /// </summary>
+    public partial class CreateBorrowingWindow : Window
+    {
+        #region Properties and Variables
+
+        #endregion
+
+        #region Constructors
+        public CreateBorrowingWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        #region AddBorrowingConfirmButton
+        private void AddBorrowingConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region AddBorrowingCancelButton
+        private void AddBorrowingCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region FromToDatePicker
+        private void FromToDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        #endregion
+        #endregion
+    }
+}
